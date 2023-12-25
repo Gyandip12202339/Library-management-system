cbf0b61f05f27b3c9038d94ccb6a64adc0fd7559
Gyandip12202339 <12202339@students.koi.edu.au>
2023-12-25 22:47:11 +1100

    Project Alpha begining

---
+++ Library management system/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library_management_system.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult AddBooks()
+        {
+            return View();
+        }
+
+        public ActionResult AddStudents()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult completeBooksDetails()
+
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Form()
+
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult IssueBooks()
+
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Form1()
+
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult issueBooks()
+
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Library management system/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Library_management_system.MvcApplication" Language="C#" %>
